19013aaf1d679ddaa5051947d7f3a9658faf9101
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2022-08-24 21:56:34 +0530

    Add files via upload

---
+++ UDE/create_table.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+
+
+font_family = "Arial"
+main_colour = "#3c096c"
+gradient_colour = ""
+text_colour =  ""
+font_size = "20"
+
+root = Tk()
+root.geometry("720x540")
+root.config(bg= main_colour)
+
+frame = Frame(root, bg = main_colour)
+
+
+heading = Label(frame, text = "TABLE FORM", bg= main_colour,font= ("Arial Bold", 24))\
+    .grid(row = 0, column = 0, columnspan=3, pady = (0,50))
+
+name = Label(frame, text="Database Name", bg = main_colour, font = (font_family, font_size))\
+    .grid(row = 1, column = 0 )
+Entry(frame, font = (font_family, font_size)).grid(row = 1, column = 1, padx = (30,0))
+
+table = Label(frame, text="Table Name", bg = main_colour, font = (font_family, font_size))\
+    .grid(row = 2, column = 0, sticky = W)
+Entry(frame, font = (font_family, font_size)).grid(row = 2, column = 1, padx = (30,0))
+
+create_button = Button(frame, text = "CREATE TABLE", font= ("Arial bold", 16), bg = "#4cc9f0")\
+    .grid(row = 3, column = 0, columnspan = 3, pady = (60,0))
+
+
+frame.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+root.mainloop()
